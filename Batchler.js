31c2e87ed5ef5012775e52f07ddbba64cdf21183
Michael Fisher <michael.fisher1988@gmail.com>
2016-01-09 14:42:16 -0800

    Update Batchler.js
    Added comments to explain the code. Still need to add more comments.

--- Batchler.js
+++ Batchler.js
@@ -1,23 +1,34 @@
 /* Batchler.js, (c) 2015, Michael Fisher, v. 1.0.0 */
+/* If the browser is an old browser like IE-9 and does not support console logging then this will prevent js errors */
 if(!this['console']) {
   this['console'] = function (message) {};
 }
-var Batchler = {};
+/* Create the Batchler object */
+var Batchler = Batchler || {};
+/* This is a sample request object. */
 Batchler.Request = function () {
+  /* A request object must contain an execute method that the request executer will call. Optionally, you may call this function manually as well.  */
   this.execute = function(executer) {
+    /* This is where your request code should go. */
     function success() {
+      /* This is where your success code should go. */
       if(executer) {
+        /* If you are using a request executer then that executer's callback function will be executed and log Success to the console. */
         executer.callback('Success');
       }
     }
     function fail() {
+      /* This is where your fail code should go. */
       if(executer) {
+        /* If you are using a request executer then that executer's callback function will be executed and log Fail to the console. */
         executer.callback('Fail');
       }
     }
   };
+  /* Return your request object. */
   return this;
 };
+/* The Batchler.Requests object is intended to house your request objects. */
 Batchler.Requests = {};
 /* Executes requests synchronously. */
 Batchler.SyncExecuter = function () {
